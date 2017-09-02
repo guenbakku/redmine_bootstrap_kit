fd37488c9fa4cf5d50b17134ab3f53a67272e481
Nguyen Van Bach <vanbach1213@gmail.com>
2017-09-02 22:15:08 +0900

    Release 0.2.6

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_bootstrap_kit do
-  name 'Redmine Bootstrap Kit plugin'
+  name 'Redmine Bootstrap Kit plugin (Forked by guenbakku)'
   author 'Nicolas Rodriguez'
   description 'This is a Bootstrap Kit plugin for Redmine'
-  version '0.2.5'
+  version '0.2.6'
   url 'https://github.com/jbox-web/redmine_bootstrap_kit'
   author_url 'https://github.com/jbox-web'
 end
